a5460ffa42fa32b90920ad0f1bc26153f73f87e6
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-11-11 06:16:53 +0800

    test diagnosis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
-<h1 class="text-red-500">Test</h1>
-<!-- <script>
+<script>
   import Header from "$lib/components/landing/header.svelte";
   import Hero from "$lib/components/landing/hero.svelte";
 </script>
 
-<main class="flex flex-col gap-40">
+<h1 class="text-red-500">Test</h1>
+
+<section class="flex flex-col gap-40">
   <Header />
   <Hero />
-</main> -->
+</section>
