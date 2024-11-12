7aaa324d6eedca7c2eaa56b61480ab4fcb142837
0.8lili <130875766+reesenuts@users.noreply.github.com>
2024-11-12 15:06:15 +0800

    footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import Header from "$lib/components/landing/header.svelte";
   import Hero from "$lib/components/landing/hero.svelte";
+  import Footer from "$lib/components/landing/footer.svelte";
 </script>
 
 
 <section class="flex flex-col gap-40">
     <Header />
     <Hero />
+    <Footer />
 </section>
-    
-<h1 class="text-red-500">Test</h1>
