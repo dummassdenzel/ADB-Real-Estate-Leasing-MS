fe59b75b7d67cf61b6256fce59718ff29530a278
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-11-13 17:50:42 +0800

    Test Scalable API Fetching

--- src/lib/components/landing/header.svelte
+++ src/lib/components/landing/header.svelte
@@ -1,20 +1,32 @@
 <header class="flex justify-between">
-    <!-- logo of aricio bonina real estate leasing -->
-    <div class="flex items-center gap-2.5">
-        <div class="w-3 h-9 bg-teal rounded-full"></div>
-        <h1 class="font-semibold text-midnight text-xs flex flex-col">aricio<span class="text-teal">real estate</span></h1>
-    </div>
+  <!-- logo of aricio bonina real estate leasing -->
+  <div class="flex items-center gap-2.5">
+    <div class="w-3 h-9 bg-teal rounded-full"></div>
+    <h1 class="font-semibold text-midnight text-xs flex flex-col">
+      aricio<span class="text-teal">real estate</span>
+    </h1>
+  </div>
 
-    <!-- navigation bar -->
-    <nav class="flex items-center gap-10">
-        <a href="" class="font-semibold text-slate text-xs hover:text-midnight">Home</a>
-        <a href="" class="font-semibold text-slate text-xs hover:text-midnight">About</a>
-        <a href="" class="font-semibold text-slate text-xs hover:text-midnight">Features</a>
-        <a href="" class="font-semibold text-slate text-xs hover:text-midnight">Contact</a>
-    </nav>
+  <!-- navigation bar -->
+  <nav class="flex items-center gap-10">
+    <a href="" class="font-semibold text-slate text-xs hover:text-midnight"
+      >Home</a
+    >
+    <a href="" class="font-semibold text-slate text-xs hover:text-midnight"
+      >About</a
+    >
+    <a href="" class="font-semibold text-slate text-xs hover:text-midnight"
+      >Features</a
+    >
+    <a href="" class="font-semibold text-slate text-xs hover:text-midnight"
+      >Contact</a
+    >
+  </nav>
 
-    <!-- login button -->
-    <div class="border-2 border-slate p-2 w-32 rounded-full text-center flex justify-center cursor-pointer">
-        <a href="/ADB-Real-Estate-Leasing-MS/login" class="font-semibold text-slate text-xs">Login</a> <!-- redirect to login -->
-    </div>
+  <!-- login button -->
+  <a
+    href="/ADB-Real-Estate-Leasing-MS/login"
+    class="border-2 border-slate p-2 w-32 rounded-full text-center flex justify-center cursor-pointer font-semibold text-slate text-xs"
+    >Login</a
+  >
 </header>
